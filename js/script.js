fe0c5284c5ca7ac1ac9cf17b1c1fbb1df230a71e
Mateusz Kretowski <matkretkurs@gmail.com>
2022-11-04 23:13:14 +0100

    Add script for player move

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-let computerMove = `kamień`;
-let playerMove = 'papier';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}else if (playerInput == '2') {
+	printMessage('papier');
+} else {
+	printMessage('nożyce');
+}
+
+printMessage('Twój ruch to: ' + playerMove);
