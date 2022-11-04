84c3ed05fb791e558d53b66d5dd933a231c557a1
Mateusz Kretowski <matkretkurs@gmail.com>
2022-11-04 23:36:49 +0100

    Add script for result

--- js/script.js
+++ js/script.js
@@ -31,3 +31,15 @@ if(playerInput == '1'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+}else if (computerMove == 'papier' && playerMove == 'nożyce') {
+    printMessage('Ty wygrywasz!');
+}else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+    printMessage('Ty wygrywasz!');
+}else if (computerMove == playerMove) {
+    printMessage('Remis');
+}else {
+    printMessage('Komputer wygrywa');
+}
