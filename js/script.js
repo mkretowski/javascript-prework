345a27cf8eb21f57b335bbc0bfb1294b34d73c86
Mateusz Kretowski <matkretkurs@gmail.com>
2022-11-06 15:51:25 +0100

    Add function doMultipleRounds

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const STONE = "kamień";
 const PAPER = "papier";
 const SCISSORS = "nożyce";
+let X = 0;
+let Y = 0;
+let roundNumber = 0;
 
 function getMoveName(argMoveId) {
   if (argMoveId == 1) {
@@ -23,6 +26,7 @@ function getResult(argComputerMove, argPlayerMove) {
     (argComputerMove == PAPER && argPlayerMove == SCISSORS) ||
     (argComputerMove == SCISSORS && argPlayerMove == STONE)
   ) {
+    X = X + 1;
     return "Ty wygrywasz!";
   }
   if (argComputerMove == argPlayerMove) {
@@ -34,14 +38,14 @@ function getResult(argComputerMove, argPlayerMove) {
   if (argComputerMove == "nieznany ruch") {
     return "Nieznany ruch komputera";
   }
-
+  Y = Y + 1;
   return "Komputer wygrywa";
 }
 
 function playGame(argplayerInput) {
   clearMessages();
   let playerInput = argplayerInput;
-  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  let randomNumber = Math.floor(Math.random() * 1.35 + 1.85);
   console.log("Wylosowana liczba to: " + randomNumber);
   let computerMove = getMoveName(randomNumber);
   printMessage("Mój ruch to: " + computerMove);
@@ -50,7 +54,28 @@ function playGame(argplayerInput) {
   let playerMove = getMoveName(playerInput);
   printMessage("Twój ruch to: " + playerMove);
 
-  printMessage("Wynik: " + getResult(computerMove, playerMove));
+  roundNumber = roundNumber + 1;
+  printMessage("Wynik rundy: " + getResult(computerMove, playerMove));
+  printMessage(
+    "Wynik po " +
+      roundNumber +
+      " rundzie: <br>Komputer: " +
+      Y +
+      " <br>Gracz: " +
+      X
+  );
+  printMessage(
+    "Procent przegranych rund przez komputer: " +
+      Math.round((X / roundNumber) * 100) +
+      "%"
+  );
+}
+
+function doMultipleRounds() {
+  for (let i = 0; i < 10000; i++) {
+    playGame(3);
+    //playGame(Math.floor(Math.random() * 3 + 1));
+  }
 }
 
 document.getElementById("play-rock").addEventListener("click", function () {
@@ -62,3 +87,8 @@ document.getElementById("play-paper").addEventListener("click", function () {
 document.getElementById("play-scissors").addEventListener("click", function () {
   playGame(3);
 });
+document
+  .getElementById("play-multiplerounds")
+  .addEventListener("click", function () {
+    doMultipleRounds();
+  });
